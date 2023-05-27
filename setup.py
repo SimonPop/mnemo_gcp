c3a3eb4fa1358237c02a38cf835afd761863ede1
Simon Popelier <simon.popelier@gmail.com>
2023-05-27 15:46:51 +0200

    Adding yaml files to package_data

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     keywords = "example documentation tutorial",
     packages=find_packages(), # ['src', 'src.pairing', 'src.pairing.search', 'src.pairing.utils'],
     package_data={
-      'src': ['**.yaml', 'src/pairing/training/config.yaml'],
+      'src': ['**.yaml', 'pairing/training/config.yaml'],
    },
     include_package_data=True,
 )
