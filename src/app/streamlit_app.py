3e8b1f166ee7e38c01ef62e0786ddc17f012fe79
Simon Popelier <simon.popelier@gmail.com>
2023-06-11 11:39:10 +0200

    :bug: Fixed bug that resets the app when trying to generate key-phrases.

--- src/app/streamlit_app.py
+++ src/app/streamlit_app.py
@@ -19,21 +19,32 @@ def load_handlers():
 def read_style():
     st.markdown(STYLE, unsafe_allow_html=True)
 
+def key_phrase_form(key: str, match: str, emoji: str):
+    """Displays a key-phrase form. This form asks for a prompt and provides a button for generating key-phrases.
+    """
+    with st.expander(f"{emoji} - {match}"):
+        with st.form(key=key):
+            prompt = st.text_input("Key-phrase prompt: ", value="It is said that ")            
+            submit = st.form_submit_button(":sparkles: Generate key-phrase!")  
+        if submit or (key + "submit" in st.session_state and st.session_state[key + "submit"]):
+            st.session_state[key + "submit"] = True
+            html_key_phrases = key_phrase_handler.generate(match, translation, prompt)
+            for index_2, html_key_phrase in enumerate(html_key_phrases):
+                st.markdown(html_key_phrase, unsafe_allow_html=True)
+
 def display_candidates(response, translation, pinyin):
+    """
+    Displays candidates mnemo words in a list.
+    For each of them, creates a form for generating key-phrases.
+    """
+
     legend = ':tropical_fish: - Definitely Mnemo / :fish: - Not exactly Mnemo / :blowfish: - Barely Mnemo'
     matches, scores = search_handler.unpack(response)
     emojis = [SearchHandler.score2emoji(x) for x in scores]
 
     for index, (match, emoji) in enumerate(zip(matches, emojis)):
         key = f"generator_{index}"
-
-        with st.expander(f"{emoji} - {match}"):
-            prompt = st.text_input("Key-phrase prompt: ", value="It is said that ", key=key + "_text")            
-            submit = st.button(":sparkles: Generate key-phrase!", key=key + "_button")     
-            if submit:
-                html_key_phrases = key_phrase_handler.generate(match, translation, prompt)
-                for index_2, html_key_phrase in enumerate(html_key_phrases):
-                    st.markdown(html_key_phrase, unsafe_allow_html=True)
+        key_phrase_form(key, match, emoji)
 
     st.info(legend)
 
@@ -49,7 +60,11 @@ input_word = placeholder.text_input("Mandarin word:")
 click_random = st.button(label="Random word")
 
 if click_random:
-    input_word = placeholder.text_input("Mandarin word:", value=input_handler.random_value(), key=1)
+    st.session_state["click_random"] = True
+    st.session_state["random_value"] = input_handler.random_value()
+
+if "click_random" in st.session_state and st.session_state["click_random"]:
+    input_word = placeholder.text_input("Mandarin word:", value=st.session_state["random_value"])
 
 if input_word:
     pinyin = input_handler.input_to_pinyin(input_word)
